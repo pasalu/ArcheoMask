19966fa9607af5b5dcd3795b254bc32b6e0b5d3f
Peter Salu <psalua@gmail.com>
2026-01-31 21:15:31 -0800

    Move triangle to where the player clicked.

--- Assets/Scripts/Stencil.cs
+++ Assets/Scripts/Stencil.cs
@@ -6,6 +6,16 @@ public class Stencil : MonoBehaviour
     public InputAction clickAction;
     public InputAction moveAction;
 
+    private InputSystem_Actions input;
+
+    void Awake()
+    {
+        // Not sure why it's necessary to call enable, but without it the input doesn't work.
+        input = new InputSystem_Actions();
+        input.Player.Enable();
+        input.UI.Enable();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,10 +25,9 @@ public class Stencil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (clickAction.triggered)
+        if (input.Player.Attack.WasPressedThisFrame())
         {
             Move();
-            print("Moved Stencil to: " + GetComponent<Transform>().position);
         }
         
     }
@@ -26,10 +35,15 @@ public class Stencil : MonoBehaviour
     // Move the stencil like it's on a grid.
     private void Move() 
     {
+        // Switch from (0,0) bottom-left to (0,0) center
+        var screenPosition = input.UI.Point.ReadValue<Vector2>();
+        var worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        print("Move: " + input.UI.Point.ReadValue<Vector2>());
         GetComponent<Transform>().position = new Vector3(
-            Mathf.Round(GetComponent<Transform>().position.x),
-            Mathf.Round(GetComponent<Transform>().position.y),
-            Mathf.Round(GetComponent<Transform>().position.z)
+            worldPosition.x,
+            worldPosition.y,
+            GetComponent<Transform>().position.z
         );
     }
 }
